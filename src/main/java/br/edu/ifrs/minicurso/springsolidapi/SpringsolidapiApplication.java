07912e3ce119b83efd7218752386c5b64f7da894
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-05 22:32:44 +0000

    springsolidapi

---
+++ src/main/java/br/edu/ifrs/minicurso/springsolidapi/SpringsolidapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.minicurso.springsolidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsolidapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsolidapiApplication.class, args);
+	}
+
+}
